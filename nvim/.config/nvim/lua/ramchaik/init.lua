10c4ea1b4525c43542b6d2c59cef35d8401fecb9
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-01-27 20:45:32 +0530

    hack: eslint lsp issue with jsdoc

--- nvim/.config/nvim/lua/ramchaik/init.lua
+++ nvim/.config/nvim/lua/ramchaik/init.lua
@@ -24,3 +24,15 @@ if pcall(require, 'plenary') then
     return require(name)
   end
 end
+
+-- Fix for eslint lsp; until next release; 
+-- eslint: https://github.com/microsoft/vscode-eslint/issues/1393
+-- neovim: https://github.com/neovim/neovim/issues/16673#issuecomment-997222902
+vim.diagnostic.set = (function(orig)
+    return function(namespace, bufnr, diagnostics, opts)
+        for _, v in ipairs(diagnostics) do
+            v.col = v.col or 0
+        end
+        return orig(namespace, bufnr, diagnostics, opts)
+    end
+end)(vim.diagnostic.set)
