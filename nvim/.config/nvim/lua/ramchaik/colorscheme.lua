2af8f4fea29e062a82abb44f02ec164af7299e63
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-07-28 09:39:36 +0530

    chore: update stuff

--- nvim/.config/nvim/lua/ramchaik/colorscheme.lua
+++ nvim/.config/nvim/lua/ramchaik/colorscheme.lua
@@ -1,5 +1,5 @@
-local colorscheme = "gruvbox"
--- local colorscheme = "tokyonight"
+-- local colorscheme = "gruvbox"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
@@ -9,5 +9,5 @@ end
 vim.opt.background = "dark"
 
 -- transparent background
--- vim.cmd([[ hi Normal guibg=NONE ctermbg=NONE ]])
+vim.cmd([[ hi Normal guibg=NONE ctermbg=NONE ]])
 
