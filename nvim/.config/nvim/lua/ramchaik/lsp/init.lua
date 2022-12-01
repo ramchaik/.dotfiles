d80a33a4ac576c37ea083c9b87299d7da0a4480a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-12-01 22:47:57 +0530

    feat: try new config

--- nvim/.config/nvim/lua/ramchaik/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "ramchaik.lsp.lsp-installer"
-require("ramchaik.lsp.handlers").setup()
-require "ramchaik.lsp.null-ls"
