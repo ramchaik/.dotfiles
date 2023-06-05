e77bc613979a03a44376801e363ca734b5c223d8
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2023-06-05 18:42:03 +0530

    chore(nvim): update config

---
+++ nvim/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "javascript", "typescript", "json", "html", "css" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
