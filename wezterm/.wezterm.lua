02ea0e38b52223c10db01625f37047431ef5041a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-09-23 14:19:34 -0300

    feat(wezterm): add config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 16
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.color_scheme = "Rosé Pine (base16)"
+
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 10
+
+return config
