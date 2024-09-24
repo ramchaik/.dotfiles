0a306039c8b3ea2c561e8351af9325faf631e950
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-09-24 20:07:15 -0300

    feat(wezterm): add wallpaper to config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,9 +10,31 @@ config.font_size = 16
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
+local function get_random_wallpaper()
+	local WALLPAPERS_BASE_PATH = "/Users/vaibhavsingh/Documents/wallpapers/"
+	local wallpapers = {}
+	local p = io.popen("ls '" .. WALLPAPERS_BASE_PATH .. "'")
+	for file in p:lines() do
+		table.insert(wallpapers, file)
+	end
+	p:close()
+	local maxIdx = #wallpapers
+	local rndIdx = math.random(1, maxIdx)
+	return WALLPAPERS_BASE_PATH .. wallpapers[rndIdx]
+end
+
+config.background = {
+	{
+		source = {
+			File = get_random_wallpaper(),
+		},
+		hsb = { hue = 1.0, saturation = 1.0, brightness = 0.03 },
+	},
+}
+
 config.color_scheme = "Catppuccin Mocha"
 
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 10
 
 return config
